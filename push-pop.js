1b9ec81c35ad1e825774e2fc94f9053148e81deb
Neeraj Tiwari <79464162+CodingExpertNeeraj@users.noreply.github.com>
2022-02-09 21:44:48 -0500

    Add files via upload

---
+++ push-pop.js
@@ -0,0 +1,21 @@
+var myArray = [23,42,78,99,45,53,34,76,86,95]
+console.log("This is my orignal array" + "  : " + myArray)
+myArray.push(100,11,39,73)
+console.log("This is my array after pushing four element" + " : " + myArray  )
+
+//Now we will do pop some item from this Array
+console.log("This is my new array" + "  : " + myArray)
+myArray.pop() 
+// 73 will out from this Array
+console.log("This is my array after pop element" + "  : " + myArray)
+
+var a=[1,2,3,4,5]
+
+a.shift()
+
+console.log(a)
+
+a.pop()
+
+console.log(a)
+
